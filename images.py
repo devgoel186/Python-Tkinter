7d4e9c4226ac1b86ca781220607eb217b2356730
Dev Goel <goeldev01@gmail.com>
2020-12-30 02:13:21 +0530

    Added functional status bar for image number

--- images.py
+++ images.py
@@ -13,6 +13,17 @@ my_img5 = ImageTk.PhotoImage(Image.open("./src/img5.jpg"))
 
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 
+# Status bar for current image number
+status = Label(root, text="Image 1 of " +
+               str(len(image_list)), bd=4, relief=SUNKEN, anchor=E)
+
+# TYPES OF RELIEF
+# B1 = Button(top, text="FLAT", relief=FLAT)
+# B2 = Button(top, text="RAISED", relief=RAISED)
+# B3 = Button(top, text="SUNKEN", relief=SUNKEN)
+# B4 = Button(top, text="GROOVE", relief=GROOVE)
+# B5 = Button(top, text="RIDGE", relief=RIDGE)
+
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
 
@@ -33,6 +44,10 @@ def forward(image_no):
     button_next.grid(row=1, column=2)
     my_label.grid(row=0, column=0, columnspan=3)
 
+    status = Label(root, text="Image " + str(image_no) + " of " +
+                   str(len(image_list)), bd=4, relief=SUNKEN, anchor=E)
+    status.grid(row=3, columnspan=3, sticky=W+E)  # Sticky from west to east
+
 
 def backward(image_no):
     global my_label, button_next, button_prev, image_list
@@ -50,12 +65,17 @@ def backward(image_no):
     button_next.grid(row=1, column=2)
     my_label.grid(row=0, column=0, columnspan=3)
 
+    status = Label(root, text="Image " + str(image_no) + " of " +
+                   str(len(image_list)), bd=4, relief=SUNKEN, anchor=E)
+    status.grid(row=3, columnspan=3, sticky=W+E)  # Sticky from west to east
+
 
 button_prev = Button(root, text="<<", state=DISABLED)
 button_quit = Button(root, text="Exit Program", command=root.quit)
 button_next = Button(root, text=">>", command=lambda: forward(2))
 button_prev.grid(row=1, column=0)
-button_quit.grid(row=1, column=1)
+button_quit.grid(row=1, column=1, pady=10)
 button_next.grid(row=1, column=2)
+status.grid(row=3, columnspan=3, sticky=W+E)  # Sticky from west to east
 
 root.mainloop()
