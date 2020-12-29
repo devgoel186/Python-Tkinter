f0a48ae7fe08b278cd5e4bfb32619c299a920648
Dev Goel <goeldev01@gmail.com>
2020-12-29 23:43:02 +0530

    Added Grid logic in tkinter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python_Tkinter
+
 Understanding Python's Tkinter to build GUIs.
--- grid.py
+++ grid.py
@@ -2,20 +2,23 @@ from tkinter import *
 
 root = Tk()
 
-e = Entry(root, width = 50, bg = "blue", fg = "white", borderwidth = 5)
+e = Entry(root, width=50, bg="blue", fg="white", borderwidth=5)
 e.pack()
 e.insert(0, "Enter Your Name: ")
 # e.get() // To get the value of the value in the entry field e
 
+
 def myClick():
     hello = "Hello " + e.get()
-    myLabel = Label(root, text = hello)
+    myLabel = Label(root, text=hello)
     myLabel.pack()
+
 
 # myButton = Button(root, text = "Click Me!", state=DISABLED, padx = 50, pady = 50)
 # myButton = Button(root, text = "Click Me!", command = myClick, fg = "yellow", bg = "#000000")
-myButton = Button(root, text = "Enter", command = myClick, fg = "yellow", bg = "#000000")
-myButton.pack() # Packs the button in the window 
+myButton = Button(root, text="Enter", command=myClick,
+                  fg="yellow", bg="#000000")
+myButton.pack()  # Packs the button in the window
 
 # Creating a Label Widget
 # myLabel1 = Label(root, text = "Hello World!").grid(row = 0, column = 0)
