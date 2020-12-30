3173028989ba707bd75b4cc3c00977c7a5c5a56a
Dev Goel <goeldev01@gmail.com>
2020-12-30 18:31:04 +0530

    Added program for multiple windows using Tkinter

---
+++ windows.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+root = Tk()
+root.title("Hello there!")
+
+# Toplevel() will create a second window alongwith the main root window
+top = Toplevel()
+top.title("My Second Window!")
+
+
+mainloop()
