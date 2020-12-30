fa6c8bf642078cd0adbe84f1fce15c40ebd989b3
Dev Goel <goeldev01@gmail.com>
2020-12-30 22:37:08 +0530

    Modified sliders.py

--- sliders.py
+++ sliders.py
@@ -8,6 +8,15 @@ root.geometry("400x400")
 vertical = Scale(root, from_=0, to=400)
 vertical.pack()
 
-horizontal = Scale(root, from_=0, to=400, orient=HORIZONTAL).pack()
+horizontal = Scale(root, from_=0, to=400, orient=HORIZONTAL)
+horizontal.pack()
+
+
+def slide():
+    # my_label = Label(root, text=horizontal.get()).pack()
+    root.geometry(str(horizontal.get()) + "x" + str(vertical.get()))
+
+
+my_btn = Button(root, text="Click Me!", command=slide).pack()
 
 mainloop()
