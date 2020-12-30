14684cb12aa75d2609b83d0c5459dfca748ef329
Dev Goel <goeldev01@gmail.com>
2020-12-30 17:17:39 +0530

    Added radio buttons with labels

--- radio.py
+++ radio.py
@@ -4,8 +4,20 @@ root = Tk()
 root.title("Radio Buttons")
 
 r = IntVar()
+r.set("0")
 
-Radiobutton(root, text="Option 1", variable=r, value=1).pack()
-Radiobutton(root, text="Option 2", variable=r, value=2).pack()
+
+def clicked(value):
+    myLabel = Label(root, text=value)
+    myLabel.pack()
+
+
+Radiobutton(root, text="Option 1", variable=r,
+            value=1, command=lambda: clicked(1)).pack()
+Radiobutton(root, text="Option 2", variable=r,
+            value=2, command=lambda: clicked(2)).pack()
+
+# myLabel = Label(root, text=r.get())
+# myLabel.pack()
 
 root.mainloop()
