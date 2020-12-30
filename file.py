57c857d9188225f1b66918094b96b7adae1635ee
Dev Goel <goeldev01@gmail.com>
2020-12-30 22:21:43 +0530

    file.py set up to select & open any image

--- file.py
+++ file.py
@@ -5,8 +5,17 @@ from tkinter import filedialog
 root = Tk()
 root.title("Open any file!")
 
-root.filename = filedialog.askopenfilename(initialdir="./src", title="Select a file!", filetypes=(
-    ("PNG Files", "*.png"), ("JPG Files", "*.jpg"), ("All Files", "*.*")))
-Label(root, text=root.filename).pack()
+
+def open():
+    global my_img
+    root.filename = filedialog.askopenfilename(initialdir="./src", title="Select a file!", filetypes=(
+        ("PNG Files", "*.png"), ("JPG Files", "*.jpg"), ("All Files", "*.*")))
+    Label(root, text=root.filename).pack()
+    my_img = ImageTk.PhotoImage(Image.open(root.filename))
+    label = Label(image=my_img).pack()
+
+
+my_btn = Button(root, text="Open File", command=open).pack()
+
 
 mainloop()
