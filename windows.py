8fcb543974a79763a60b4e4da323c69191730edd
Dev Goel <goeldev01@gmail.com>
2020-12-30 18:37:40 +0530

    Added opening and closing buttons for multiple windows

--- windows.py
+++ windows.py
@@ -5,9 +5,15 @@ from PIL import Image, ImageTk
 root = Tk()
 root.title("Hello there!")
 
-# Toplevel() will create a second window alongwith the main root window
-top = Toplevel()
-top.title("My Second Window!")
 
+# Toplevel() will create a second window alongwith the main root window
+def open():
+    top = Toplevel()
+    top.title("My Second Window!")
+    Button(top, text="Close Window!", command=top.destroy).pack(padx=20, pady=20)
+
+
+btn = Button(root, text="Open Second Window",
+             command=open).pack(padx=50, pady=50)
 
 mainloop()
