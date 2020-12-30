3a47c16fee3e6f77c4c2197ac9180e372a310072
Dev Goel <goeldev01@gmail.com>
2020-12-30 17:37:20 +0530

    Added radio buttons via loop

--- radio.py
+++ radio.py
@@ -6,16 +6,36 @@ root.title("Radio Buttons")
 r = IntVar()
 r.set("0")
 
+list_frame = LabelFrame(
+    root, text="Types of Pizzas Available", padx=50, pady=50)
+list_frame.pack(padx=10, pady=10)
+
+MODES = [
+    ("Pepperoni", "Pepperoni"),
+    ("Cheese", "Cheese"),
+    ("Mushroom", "Mushroom"),
+    ("Onion", "Onion"),
+]
+
+pizza = StringVar()
+pizza.set("Pepperoni")
+
+for text, mode in MODES:
+    Radiobutton(list_frame, text=text, variable=pizza,
+                value=mode).pack(anchor=W)
+
 
 def clicked(value):
-    myLabel = Label(root, text=value)
+    myLabel = Label(root, text="Your selected type = " + value)
     myLabel.pack()
 
 
-Radiobutton(root, text="Option 1", variable=r,
-            value=1, command=lambda: clicked(1)).pack()
-Radiobutton(root, text="Option 2", variable=r,
-            value=2, command=lambda: clicked(2)).pack()
+# Radiobutton(root, text="Option 1", variable=r,
+#             value=1, command=lambda: clicked(1)).pack()
+# Radiobutton(root, text="Option 2", variable=r,
+#             value=2, command=lambda: clicked(2)).pack()
+
+Button(root, text="Click Me!", command=lambda: clicked(pizza.get())).pack(pady=10)
 
 # myLabel = Label(root, text=r.get())
 # myLabel.pack()
