e7ac1bd81d0c082e5db3c93d19b67542ac5e1659
Dev Goel <goeldev01@gmail.com>
2020-12-31 19:21:48 +0530

    Minor changes to databases.py

--- databases.py
+++ databases.py
@@ -12,15 +12,23 @@ conn = sqlite3.connect("address_book.db")
 # Create cursor
 c = conn.cursor()
 
-c.execute("""CREATE TABLE addresses (
-    first_name text,
-    last_name text, 
-    address text, 
-    city text, 
-    state text, 
-    zipcode integer
-)
-""")
+# c.execute("""CREATE TABLE addresses (
+#     first_name text,
+#     last_name text,
+#     address text,
+#     city text,
+#     state text,
+#     zipcode integer
+# )
+# """)
+
+f_name = Entry(root, width=30).grid(row=0, column=0, padx=20)
+l_name = Entry(root, width=30).grid(row=1, column=0, padx=20)
+address = Entry(root, width=30).grid(row=2, column=0, padx=20)
+city = Entry(root, width=30).grid(row=3, column=0, padx=20)
+state = Entry(root, width=30).grid(row=4, column=0, padx=20)
+zipcode = Entry(root, width=30).grid(row=5, column=0, padx=20)
+
 
 # Commit our changes
 conn.commit()
